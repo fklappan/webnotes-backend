53e09a50a6c50710cadda188b7ce79c8e35f308f
Florian Klappan <florian.klappan@googlemail.com>
2019-11-11 21:08:33 +0100

    Revert "added nginx header access workaround"
    This reverts commit fb4d6d272f13b4ccae483f7d0c0dca5df66defbd.

--- init.php
+++ init.php
@@ -1,7 +1,6 @@
 <?php
 require __DIR__ . "/autoload.php";
 require __DIR__ . "/version.php";
-require __DIR__ . "/nginx_workaround.php";
 $config = parse_ini_file("config.ini.php");
 $container = new App\Core\Container($config);
 
