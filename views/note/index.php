653ebca210bd18fbe01d9034a2ac832363f95a1b
Florian Klappan <florian.klappan@googlemail.com>
2020-02-26 20:23:06 +0100

    slightly reworked layout

--- views/note/index.php
+++ views/note/index.php
@@ -3,25 +3,34 @@
 <h1>Übersicht der Notizen</h1>
 <p class="lead">Hier sind alle Notizen hinterlegt</p>
 
+
+<div class="container-fluid">
+<div class="row">
 <?php foreach ($notes as $note) : ?>
-<div class="col">
-    <div class="card w-95">
-        <div class="card-body">
-            <h4 class="card-title"><?php echo $note->title; ?></h4>
-            <p class="card-text"><?php echo nl2br($note->content); ?></p>
-            <!-- <a href="#" class="btn btn-outline-primary" onclick="changeState(this)">Favorit</a>-->
-            <div class ="row">
-                <a href="edit?id=<?php echo $note->id; ?>" class="btn btn-primary mx-3">Edit Note</a> 
-                <form method="post" action="deletenote?id=<?php echo $note->id; ?>">
-                    <input type="submit" class="btn btn-danger" value="Delete Note">
-                </form>
+    <div class="col-6">
+        <div class="card w-95">
+            <div class="card-body">
+                <h4 class="card-title"><?php echo $note->title; ?></h4>
+                <p class="card-text" style="max-height: 300px; overflow: hidden; text-overflow: ellipsis;"><?php echo nl2br($note->content); ?></p>
+                <div class ="row">
+                <div class ="col-md-6">
+                    <a href="edit?id=<?php echo $note->id; ?>" class="btn btn-primary mx-3">Edit Note</a> 
+                    </div>
+                    <div class ="col-md-6">
+                    <form method="post" action="deletenote?id=<?php echo $note->id; ?>">
+                        <input type="submit" class="btn btn-danger" value="Delete Note">
+                    </form>
+                    </div>
+                </div>
+                
+                <!-- <a href="deletenote?id=<?php echo $note->id; ?>" class="btn btn-danger">Delete Note</a> -->
             </div>
-            
-            <!-- <a href="deletenote?id=<?php echo $note->id; ?>" class="btn btn-danger">Delete Note</a> -->
         </div>
     </div>
+    <?php endforeach ?>
+</div>
 </div>
 <br>
-<?php endforeach ?>
+
 
 <?php include __DIR__ . "/../layout/footer.php"; ?>
